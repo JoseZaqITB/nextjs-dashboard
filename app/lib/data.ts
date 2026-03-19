afa0fb07237ec5d8b002bcb7957567e496118b5f
yoseyusMainComputer <zaquijobs@gmail.com>
2026-03-19 09:18:37 +0100

    added code until chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,6 +8,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { resolve } from 'path';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -16,12 +17,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+     console.log('Fetching revenue data...');
+     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -43,6 +44,8 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+    // temp
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -73,6 +76,7 @@ export async function fetchCardData() {
     const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2][0].pending ?? '0');
 
+    await new Promise((resolve) => setTimeout(resolve, 1500));
     return {
       numberOfCustomers,
       numberOfInvoices,
